3a1049c79ad543caa4da23ba2a42728cc1e10521
Amelia Trainer <ameliajo@mit.edu>
2017-11-04 07:20:19 -0400

    improving tests in start and convol_util

--- contin/contin_util/convol.test.cpp
+++ contin/contin_util/convol.test.cpp
@@ -4,47 +4,61 @@
 #include <iostream>
 
 void equal( double a, double b ){
-    if (b == 0.0){ 
-        REQUIRE( b-a < 1e-6 );
-        return;
-    }
-    REQUIRE ( std::abs( (a-b)/(b) ) < 1e-6 );
+  if (b == 0.0){ 
+    REQUIRE( b-a < 1e-6 );
+    return;
+  }
+  REQUIRE ( std::abs( (a-b)/(b) ) < 1e-6 );
 }
 
 void equal_vec( std::vector<double> a, std::vector<double> b ){
-    REQUIRE( a.size() == b.size() );
-    for ( int i = 0; i < a.size(); ++i ){
-        equal( a[i], b[i] );
-    }
+  REQUIRE( a.size() == b.size() );
+  for ( int i = 0; i < a.size(); ++i ){
+    equal( a[i], b[i] );
+  }
 }
 
 TEST_CASE( "convol" ){
-    GIVEN( "two vectors" ){
-        THEN( "correctly convolves the two" ){
-            {
-                std::vector<double> t1 {0.2, 0.6, 0.8, 2.0, 6.0, 8.0};
+  GIVEN( "two vectors" ){
+    std::vector<double> t1 {0.2, 0.6, 0.8, 2.0, 6.0, 8.0};
+    std::vector<double> t2 {0.2, 0.6, 0.8, 2.0, 6.0, 8.0,
+                            0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                            0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    double delta = 0.03;
+    THEN( "the vectors are correctly convolved and result is returned" ){
+      std::vector<double> output = convol( t1, t2, delta );
+      std::vector<double> correct = {3.8459762, 2.6993367, 1.0195307, 
+        0.53364442, 0.37281623, 0.384, 0.624, 1.008, 1.8, 
+        2.16, 0.96, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
-                std::vector<double> t2 {0.2, 0.6, 0.8, 2.0, 6.0, 8.0,
-                                        0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                                        0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-                double delta = 0.03;
+      equal_vec(output, correct);
 
-                std::vector<double> output = convol( t1, t2, delta );
-                std::vector<double> correct = {3.8459762, 2.6993367, 1.0195307, 0.53364442, 0.37281623, 0.38400000, 0.62399999, 1.0079999, 1.7999999, 2.1599999, 0.95999997854232788, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-                equal_vec(output, correct);
            
-            }
-            {
-                std::vector<double> t1 = {0.01, 0.04, 0.09, 0.11, 0.16, 0.21};
-                std::vector<double> t2 = {0.01, 0.04, 0.09, 0.11, 0.16, 0.21,
-                                          0.0,  0.0,  0.0,  0.0,  0.0,  0.0,
-                                          0.0,  0.0,  0.0,  0.0,  0.0,  0.0};
-                double delta = 0.5;
-                std::vector<double> output = convol( t1, t2, delta );
-                std::vector<double> correct {1.1974704E-2, 1.3563056E-2, 1.3531928E-2, 1.3796487E-2, 1.3871143E-2, 1.7874999E-2, 2.6749999E-2, 3.1774999E-2, 3.0124998E-2, 2.5199998E-2, 1.1024999E-2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-                equal_vec(output, correct);
-            }
-        } // THEN
-    } // GIVEN
+      t1 = {0.01, 0.04, 0.09, 0.11, 0.16, 0.21};
+      t2 = {0.01, 0.04, 0.09, 0.11, 0.16, 0.21, 0.0,  0.0,  0.0,  0.0,  0.0,  
+            0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0};
+      delta = 0.5;
+      output = convol( t1, t2, delta );
+      correct = {1.1974704E-2, 1.3563056E-2, 1.3531928E-2, 1.379648E-2, 
+        1.387114E-2, 1.7875E-2, 2.675E-2, 3.1775E-2, 3.0125E-2, 
+        2.52E-2, 1.1025E-2, 0., 0., 0., 0., 0., 0., 0.};
+
+      equal_vec(output, correct);
+
+      t1 = {0.41483349, 0.49122347, 0.28793794, 0.19807373, 0.16013178, 
+        0.35027406, 0.54943040};
+      t2 = {0.41483349, 0.49122347, 0.28793794, 0.19807373, 0.16013178, 
+        0.35027406, 0.5494304, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
+        0.0, 0.0, 0.0, 0.0, 0.0};
+      delta = 0.3481334;
+      output = convol( t1, t2, delta );
+      correct = {0.24934471, 0.26630576, 0.25941571, 0.24935982, 0.26861886, 
+        0.29515324, 0.28458452, 0.23324588, 0.1398470, 9.5883480E-2, 8.8657E-2,
+        0.1004980, 5.2546179E-2, 0., 0., 0., 0., 0., 0., 0., 0.};
+
+      equal_vec(output, correct);
+
+    } // THEN
+  } // GIVEN
 } // TEST CASE
 
--- contin/contin_util/start_util/start_util.test.cpp
+++ contin/contin_util/start_util/start_util.test.cpp
@@ -5,7 +5,7 @@
 #include <cmath>
 
 void equal( double a, double b ){
-    REQUIRE ( std::abs( (a-b)/(b) ) < 1e-7 );
+    REQUIRE ( std::abs( (a-b)/(b) ) < 1e-6 );
 }
 
 void equal_vec( std::vector<double> a, std::vector<double> b ){
@@ -16,8 +16,8 @@ void equal_vec( std::vector<double> a, std::vector<double> b ){
 }
 
 TEST_CASE( "fsum" ){
-    WHEN( " n = 1 (used for normalizing p(beta)) " ){
-        THEN( "returned value has as most a 1e-7 percent error" ){
+    WHEN( "n = 1 (used for normalizing p(beta)) " ){
+        THEN( "returned value has as most a 1e-6 percent error" ){
             std::vector<double> p = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6};
             double tau = 0.5, delta = 1.0;
             equal( fsum( 1, p, tau, delta ), 39.387006 );
@@ -28,8 +28,8 @@ TEST_CASE( "fsum" ){
 
         } // THEN
     } // WHEN
-    WHEN( " n = 0 (used for debye-waller coefficient) " ){
-        THEN( "returned value has as most a 1e-7 percent error" ){
+    WHEN( "n = 0 (used for debye-waller coefficient) " ){
+        THEN( "returned value has as most a 1e-6 percent error" ){
             std::vector<double> p = {0.01, 0.02, 0.03, 0.04, 0.05, 0.06};
             double tau = 0.5, delta = 0.1;
             equal( fsum( 0, p, tau, delta ), 0.035514341 );
@@ -40,8 +40,8 @@ TEST_CASE( "fsum" ){
 
         } // THEN
     } // WHEN
-    WHEN( " n = 2 (used for effective temperature calculation) " ){
-        THEN( "returned value has as most a 1e-7 percent error" ){
+    WHEN( "n = 2 (used for effective temperature calculation) " ){
+        THEN( "returned value has as most a 1e-6 percent error" ){
             std::vector<double> p = {0.01, 0.02, 0.03, 0.04, 0.05, 0.06};
             double tau = 0.5, delta = 0.7;
             equal( fsum( 2, p, tau, delta ), 3.21945524 );
@@ -61,7 +61,7 @@ TEST_CASE( "normalize" ){
         std::vector<double> output = normalize( p, delta, tbeta );
         std::vector<double> correct= {2.62155807E-2, 5.24311614E-2, 
               7.86467421E-2, 0.104862322, 0.1310779, 0.15729348};
-        THEN( "each value in vector has as most a 1e-7 percent error" ){
+        THEN( "each value in vector has as most a 1e-6 percent error" ){
             equal_vec( output, correct );
         } // THEN
 
@@ -69,18 +69,20 @@ TEST_CASE( "normalize" ){
              3.901418923e-2, 4.681702708e-2, 5.461986492e-2, 6.242270277e-2, 
              7.022554062e-2, 7.802837847e-2, 8.583121631e-2, 9.363405416e-2};
         delta = 0.2; tbeta = 2.0;
+        output = normalize( p, delta, tbeta );
         correct = {5.2978935E-2, 0.10595787, 0.15893680, 0.21191574, 
                    0.26489467, 0.31787361, 0.37085254, 0.42383148, 
                    0.47681042, 0.52978935, 0.58276829, 0.63574722};
+        THEN( "each value in vector has as most a 1e-6 percent error" ){
+          equal_vec( output, correct );
+        } //  THEN
 
-        output = normalize( p, delta, tbeta );
-        equal_vec( output, correct );
     } // GIVEN 
     GIVEN( "two vectors where one is the first scaled by some constant" ){
         std::vector<double> p1 = {0.02, 0.04, 0.06, 0.08, 0.10, 0.12};
         std::vector<double> p2 = {0.2, 0.4, 0.6, 0.8, 1.0, 1.2};
         double tbeta = 5.0, delta = 0.8;
-        THEN( "outputs should be same, and both within 1e-7 to true value" ){
+        THEN( "outputs should be same, and both within 1e-6 to true value" ){
             std::vector<double> output1 = normalize( p1, delta, tbeta );
             std::vector<double> output2 = normalize( p2, delta, tbeta );
             std::vector<double> correct= {3.095826174E-2, 6.191652348E-2, 
