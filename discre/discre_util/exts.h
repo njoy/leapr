1db218a49f81e2cc21f3e922cb7be2183bf08c0b
Amelia Trainer <ameliajo@mit.edu>
2017-11-09 12:06:52 -0500

    made exts more condensed and (I think) readable

--- discre/discre_util/exts.h
+++ discre/discre_util/exts.h
@@ -2,24 +2,18 @@
 #include <vector>
 
 
-auto exts( const std::vector<double>& sexpb, std::vector<double>& sex,
+void exts( const std::vector<double>& sexpb, std::vector<double>& sex,
   const std::vector<double>& exb, const std::vector<double>& beta ){
-  int k = beta.size();
-  for ( auto b = 0; b < beta.size(); ++b ){
-    sex[b] = sexpb[k-1];
-    k -= 1;
-  }
-  if ( beta[0] <= 1.0e-9 ){
-    sex[beta.size()-1] = sexpb[0];
-    k = beta.size() + 1;
-  }else{
-    k = beta.size() + 2;
-    sex[beta.size()] = sexpb[0];
-  }
+
+  // Here we reverse the vector sexpb and put it in the beginning
+  // of sex.
+  std::reverse_copy(std::begin(sexpb), std::end(sexpb), std::begin(sex) );
+
+  int k = beta[0] <= 1.0e-9 ? beta.size() + 1 : beta.size() + 2;
+  sex[k-2] = sexpb[0];
+
   for ( int b = 1; b < beta.size(); ++b ){
     sex[k-1] = sexpb[b]*exb[b]*exb[b];
     k += 1;
   }
-  return 0;
-
 }
