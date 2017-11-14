44edd4bbb8d78beb8efd9afcfb78bcbe452b0e5f
Amelia Trainer <ameliajo@mit.edu>
2017-11-14 12:47:07 -0500

    made helper function for discre positive term loop piece

--- discre/discre_util/oscLoopFuncs.h
+++ discre/discre_util/oscLoopFuncs.h
@@ -2,31 +2,42 @@
 #include <vector>
 
 
-auto negativeTerms( int i, int& n, const double& normalizedEnergy, 
-  const std::vector<double>& bminus, const int maxdd,
+void negativeTerms( int i, int& n, const double& normalizedEnergy, 
+  const std::vector<double>& bminus,
   std::vector<double>& wts, const std::vector<double>& wtn, 
-  std::vector<double>& bes, const std::vector<double>& ben, int nn){
+  std::vector<double>& bes, const std::vector<double>& ben, const int nn){
 
-  int k = 0, idone = 0;
-  double besn, wtsn;
+  int k = 0;
 
-  while ( k < 50 and idone == 0 ){
+  while ( k < 50 ){
     k += 1;
-    if ( bminus[k-1] <= 0 ){
-      idone = 1;
-    } else {
-      for ( auto m = 0; m < nn; ++m ){
-        besn = ben[m] - k * normalizedEnergy;
-        wtsn = wtn[m] * bminus[k-1];
-        if ( wtsn >= 1e-8 and n < maxdd ){
-          n += 1;
-          bes[n] = besn;
-          wts[n] = wtsn;
-        }
+    if ( bminus[k-1] <= 0 ){ return; } 
+    for ( auto m = 0; m < nn; ++m ){
+      if ( wtn[m] * bminus[k-1] >= 1e-8 and n < bes.size() ){
+        n += 1;
+        bes[n] = ben[m] - k * normalizedEnergy;
+        wts[n] = wtn[m] * bminus[k-1];
       }
     }
   }
-  //for( auto entry : bes ){ std::cout << entry << std::endl; }
-  //std::cout << wtsn << std::endl;
-  return;
+}
+
+void positiveTerms( int i, int& n, const double& normalizedEnergy, 
+  const std::vector<double>& bplus,
+  std::vector<double>& wts, const std::vector<double>& wtn, 
+  std::vector<double>& bes, const std::vector<double>& ben, const int nn){
+
+  int k = 0;
+
+  while ( k < 50 ){
+    k += 1;
+    if ( bplus[k-1] <= 0 ){ return; } 
+    for ( auto m = 0; m < nn; ++m ){
+      if ( wtn[m] * bplus[k-1] >= 1e-8 and n < bes.size() ){
+        n += 1;
+        bes[n] = ben[m] + k * normalizedEnergy;
+        wts[n] = wtn[m] * bplus[k-1];
+      }
+    }
+  }
 }
--- discre/discre_util/oscLoopFuncs.test.cpp
+++ discre/discre_util/oscLoopFuncs.test.cpp
@@ -3,6 +3,7 @@
 #include "oscLoopFuncs.h"
 
 void equal( double a, double b ){
+//  std::cout << a << "   "  << b << std::endl;
   if (b == 0.0){ 
     REQUIRE( std::abs(b-a) < 1e-6 );
     return;
@@ -12,45 +13,110 @@ void equal( double a, double b ){
 
 
 TEST_CASE( "negative n terms loop" ){
-  GIVEN( "inputs" ){
-    int i = 0, n = 0;
-    int maxdd = 500;
-    double wtsn = 0.987274731;
-    double besn = 0.0;
-    std::vector<double> bminus (50, 0.0);
-    std::vector<double> bes (maxdd, 0.0);
-    std::vector<double> wts (maxdd, 0.0);
-    wts[0] = 0.9872747318;
-    std::vector<double> wtn (maxdd, 0.0);
-    std::vector<double> ben (maxdd, 0.0);
-    wtn[0] = 1.0;
-    int nn = 1;
-    bminus[0] = 1.119176E-2; 
-    bminus[1] = 6.343540E-5; 
-    bminus[2] = 2.397033E-7; 
-    bminus[3] = 6.793257E-10; 
-    bminus[4] = 1.540182E-12; 
-    bminus[5] = 2.909946E-15; 
-    bminus[6] = 4.712496E-18; 
-    bminus[7] = 6.677675E-21; 
-    bminus[8] = 8.410987E-24;
-    double normalizedEnergy = 2.03077847;
+  int maxdd = 500;
+  std::vector<double> bminus (50, 0.0), bes (maxdd, 0.0), wts (maxdd, 0.0),
+                      wtn (maxdd, 0.0), ben (maxdd, 0.0);
+ GIVEN( "inputs" ){
+    int i = 0, n = 0, nn = 1;
+    double wtsn = 0.987274731, besn = 0.0, normEnergy = 2.03077847;
+    wts[0] = 0.9872747318, wtn[0] = 1.0;
 
-    negativeTerms( i, n, normalizedEnergy, bminus, maxdd, wts, wtn, bes, ben, nn );
+    bminus[0] = 1.119176E-2; bminus[1] = 6.343540E-5; bminus[2] = 2.397033E-7; 
+    bminus[3] = 6.79325E-10; bminus[4] = 1.54018E-12; bminus[5] = 2.909946E-15; 
+    bminus[6] = 4.71249E-18; bminus[7] = 6.67767E-21; bminus[8] = 8.410987E-24;
+
+    negativeTerms( i, n, normEnergy, bminus, wts, wtn, bes, ben, nn );
     
-    equal( n, 3 );
+    THEN( "values are correct" ){
+      equal( n, 3 );
 
-    std::vector<double> besVals {0.0, -2.030778, -4.061556, -6.092335};
-    for ( auto i = 0; i < bes.size(); ++i ){ 
-      if ( i < 4 ){ equal( bes[i], besVals[i] ); }
-      else { equal( bes[i], 0.0 ); }
-    }
+      std::vector<double> besVals {0.0, -2.030778, -4.061556, -6.092335};
+      for ( auto i = 0; i < bes.size(); ++i ){ 
+        if ( i < 4 ){ equal( bes[i], besVals[i] ); }
+        else { equal( bes[i], 0.0 ); }
+      }
 
-    std::vector<double> wtsVals {0.9872747, 1.119176E-2, 6.343540E-5, 2.397033E-7};
-    for ( auto i = 0; i < wts.size(); ++i ){
-      if ( i < 4 ){ equal( wts[i], wtsVals[i] ); }
-      else { equal( wts[i], 0.0 ); }
-    }
+      std::vector<double> wtsVals {0.9872747, 1.119176E-2, 6.343540E-5, 
+        2.397033E-7};
+      for ( auto i = 0; i < wts.size(); ++i ){
+        if ( i < 4 ){ equal( wts[i], wtsVals[i] ); }
+        else { equal( wts[i], 0.0 ); }
+      }
+    } // THEN
+  } // GIVEN
+  GIVEN( "other inputs" ){
+    std::fill( wts.begin(), wts.end(), 0.0 );
+    std::fill( bes.begin(), bes.end(), 0.0 );
+    wts[0] = 0.8; wtn[0] = 1.0;
+    int i = 0, n = 0, nn = 1;
+    double normEnergy = 2.5;
+    std::fill( bminus.begin(), bminus.end(), 0.0 );
+    bminus[0] = 1.0; bminus[1] = 2.0;
+    for ( auto i = 2; i < 10; ++i ){ bminus[i] = bminus[i-1] + bminus[i-2];}
+    
+    negativeTerms( i, n, normEnergy, bminus, wts, wtn, bes, ben, nn );
+    std::vector<double> besVals {0.0, -2.5, -5.0, -7.5, -10.0, -12.5, -15.0, 
+      -17.5, -20.0, -22.5, -25.0};
+    std::vector<double> wtsVals {0.8, 1.0, 2.0, 3.0, 5.0, 8.0, 13.0, 21.0, 
+      34.0, 55.0, 89.0};
+    
+    THEN( "values are correct" ){
+      for ( auto i = 0; i < bes.size(); ++i ){ 
+        if ( i < besVals.size() ){ equal( bes[i], besVals[i] ); }
+        else { equal( bes[i], 0.0 ); }
+      }
+      for ( auto i = 0; i < wts.size(); ++i ){
+        if ( i < wtsVals.size() ){ equal( wts[i], wtsVals[i] ); }
+        else { equal( wts[i], 0.0 ); }
+      }
 
+    } // THEN
   } // GIVEN
 } // TEST CASE
+
+
+TEST_CASE( "positive terms" ){
+  GIVEN( "inputs" ){
+    int maxdd = 500, i = 0, n = 10, nn = 1;
+    std::vector<double> bplus (50, 0.0),  bes (maxdd, 0.0), wts (maxdd, 0.0),
+                        wtn (maxdd, 0.0), ben (maxdd, 0.0);
+    double normalizedEnergy = 2.5;
+    wtn[0] = 1.0;
+    wts[0] = 0.8; wts[1] = 1.0; wts[2] = 2;
+
+    for ( auto i = 3; i < 11; ++i ){ wts[i] = wts[i-1] + wts[i-2]; }
+    for ( auto i = 0; i < 11; ++i ){ bes[i] = -i * 2.5; }
+
+    bplus[0] = 0.08;   bplus[1] = 0.06;  bplus[2] = 0.04;  bplus[3] = 0.02;
+    bplus[4] = 0.008;  bplus[5] = 0.006; bplus[6] = 0.004; bplus[7] = 0.002;
+    bplus[8] = 0.0008; bplus[9] = 0.0006;
+    positiveTerms( i, n, normalizedEnergy, bplus, wts, wtn, bes, ben, nn ); 
+    
+    THEN( "values are correct" ){
+      std::vector<double> besVals {0.0, -2.5, -5.0, -7.5, -10.0, -12.5, -15.0, 
+        -17.5, -20.0, -22.5, -25.0, 2.5, 5.0, 7.5, 10.0, 12.5, 15.0, 17.5,
+        20.0, 22.5, 25.0};
+      std::vector<double> wtsVals {0.8, 1.0, 2.0, 3.0, 5.0, 8.0, 13.0, 21.0, 
+        34.0, 55.0, 89.0, 8.0E-2, 6.0E-2, 4.0E-2, 2.0E-2, 8.0E-3, 6.0E-3, 
+        4.0E-3, 2.0E-3, 8.0E-4, 6.0E-4};
+
+      for ( auto i = 0; i < bes.size(); ++i ){ 
+        if ( i < besVals.size() ){ equal( bes[i], besVals[i] ); }
+        else { equal( bes[i], 0.0 ); }
+      }
+      
+      for ( auto i = 0; i < wts.size(); ++i ){
+        if ( i < wtsVals.size() ){ equal( wts[i], wtsVals[i] ); }
+        else { equal( wts[i], 0.0 ); }
+      }
+      
+      equal( n, 20 );
+
+
+
+
+    } // THEN
+  } // GIVEN
+} // TEST CASE
+
+
