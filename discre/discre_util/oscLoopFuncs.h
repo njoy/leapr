2e28db04133c6a7f78bb417b8f62710a3ecde245
Amelia Trainer <ameliajo@mit.edu>
2017-11-15 09:41:48 -0500

    made tests and improved the discre oscillator loop function

--- discre/discre_util/oscLoopFuncs.h
+++ discre/discre_util/oscLoopFuncs.h
@@ -12,7 +12,7 @@ void negativeTerms( int i, int& n, const double& normalizedEnergy,
   while ( k < 50 ){
     k += 1;
     if ( bminus[k-1] <= 0 ){ return; } 
-    for ( auto m = 0; m < nn; ++m ){
+    for ( auto m = 0; m <= nn; ++m ){
       if ( wtn[m] * bminus[k-1] >= 1e-8 and n < bes.size() ){
         n += 1;
         bes[n] = ben[m] - k * normalizedEnergy;
@@ -32,7 +32,7 @@ void positiveTerms( int i, int& n, const double& normalizedEnergy,
   while ( k < 50 ){
     k += 1;
     if ( bplus[k-1] <= 0 ){ return; } 
-    for ( auto m = 0; m < nn; ++m ){
+    for ( auto m = 0; m <= nn; ++m ){
       if ( wtn[m] * bplus[k-1] >= 1e-8 and n < bes.size() ){
         n += 1;
         bes[n] = ben[m] + k * normalizedEnergy;
@@ -45,51 +45,51 @@ void positiveTerms( int i, int& n, const double& normalizedEnergy,
 void oscillatorLoop( std::vector<double>& alpha,
   std::vector<double>& dbw, std::vector<double>& ar, const double& scaling,
   std::vector<double>& wts, std::vector<double>& wtn, std::vector<double>& bes,
-  std::vector<double>& ben, std::vector<double>& energyNorm, int a, int maxdd ){
-
+  std::vector<double>& ben, std::vector<double>& energyNorm, int a, int maxdd,
+  int numOscillators, double& wt, double& tbart, std::vector<double>& weight,
+  std::vector<double>& dist, double& temp ){
+  double bk = 8.617385E-5;
   int nn = 1;
-  int n = 0;
 
   // Loop over all oscillators
-  for ( auto i = 0; i < energyNorm.size(); ++i ){
+  for ( auto i = 0; i < numOscillators; ++i ){
+    int n = 0;
     double dwc = alpha[a]*scaling*dbw[i];
     double x   = alpha[a]*scaling*ar[i];
     std::vector<double> bminus (50,0.0), bplus(50,0.0);
     double bzero = bfact( x, dwc, energyNorm[i], bplus, bminus );
-
+    
     // do convolution for delta function
-    for ( auto m = 0; m < nn; ++m ){
+    for ( auto m = 0; m <= nn; ++m ){
       double besn = ben[m];
       double wtsn = wtn[m]*bzero;
-      if ( besn <= 0 or wtsn >= 1e-8 ){
+      if ( besn < 0 or wtsn >= 1e-8 ){
         if ( n < maxdd ){
-          bes[n] = besn;
+         bes[n] = besn;
           wts[n] = wtsn;
           n += 1;
         }
       }
     }
     n -= 1;
+
     // negative n terms
     negativeTerms( i, n, energyNorm[i], bminus, wts, wtn, bes, ben, nn );
 
     // positive n terms
     positiveTerms( i, n, energyNorm[i], bplus, wts, wtn, bes, ben, nn );
-    //for ( auto entry : wts ) { std::cout << entry << std::endl;  }
-    
+
     // continue loop
     nn = n;
-    for ( auto m = 0; m < nn; ++m ){
+    for ( auto m = 0; m <= nn; ++m ){
       ben[m] = bes[m];
       wtn[m] = wts[m];
     }
-    n = 0;
-   // wt = wt + weight[i];
-   // tbart = tbart + dist[i] / bk / tempr[itemp];
+    wt += weight[i];
+    tbart += dist[i] / ( bk * temp );
 
 
   }   
-  for ( auto entry : wts ) { std::cout << entry << std::endl; }
   return;
 }
 
--- discre/discre_util/oscLoopFuncs.test.cpp
+++ discre/discre_util/oscLoopFuncs.test.cpp
@@ -4,10 +4,10 @@
 
 void equal( double a, double b ){
   if (b == 0.0){ 
-    REQUIRE( std::abs(b-a) < 1e-6 );
+    REQUIRE( std::abs(b-a) < 2e-6 );
     return;
   }
-  REQUIRE ( std::abs( (a-b)/(b) ) < 1e-6 );
+  REQUIRE ( std::abs( (a-b)/(b) ) < 2e-6 );
 }
 
 
@@ -118,34 +118,52 @@ TEST_CASE( "positive terms" ){
 
 TEST_CASE( "oscillator loop" ){
   GIVEN( "inputs" ){
-    std::vector<double> wtsCorrect {0.9573911, 1.085300E-2, 6.151529E-5, 
-      2.324478E-7, 1.424275E-3, 1.059427E-6, 2.793543E-2, 3.166766E-4, 
-      1.794936E-6, 4.155853E-5, 3.091273E-8, 4.075663E-4, 4.620180E-6, 
-      2.618737E-8, 6.063216E-7, 3.964163E-6, 4.493784E-8, 2.891790E-8, 
-      1.535389E-3, 1.740520E-5, 9.865342E-8, 2.284142E-6, 1.231187E-6, 
-      1.395677E-8};
+    int maxdd = 500, numOscillators = 2, a = 0;
 
-    int maxdd = 500;
+    double scaling = 1.0, wt = 2.0, tbart = 405.894676, temp = 200.0;
+
     std::vector<double> wts (maxdd, 0.0), bes(maxdd, 0.0), energyNorm(50, 0.0), 
-      dbw(50, 0.0), wtn(maxdd, 0.0), ben(maxdd, 0.0), ar(50, 0.0);
+      dbw(50, 0.0), wtn(maxdd, 0.0), ben(maxdd, 0.0), ar(50, 0.0), dist(50,0.0);
+
     energyNorm[0] = 2.030778; energyNorm[1] = 2.901112;
+    dist[0] = 4.55739E-3; dist[1] = 2.232634E-2;
     dbw[0] = 0.128237; dbw[1] = 0.307831;
-    wtn[0] = 1.0;
-    double scaling = 1.0; int a = 0;
     ar[0] = 8.213274E-2; ar[1] =  0.1368162;
+    wtn[0] = 1.0;
+
     std::vector<double> alpha {0.1, 0.2, 0.4, 0.8, 1.6};
-    oscillatorLoop( alpha,  
+    std::vector<double> weight{0.2, 0.8};
 
+    oscillatorLoop( alpha, dbw, ar, scaling, wts, wtn, bes, ben, energyNorm, 
+      a, maxdd, numOscillators, wt, tbart, weight, dist, temp );
 
+    THEN( "ouput is correct" ){
+      std::vector<double> wtsCorrect {0.9573911, 1.085300E-2, 6.151529E-5, 
+        2.324478E-7, 1.424275E-3, 1.0594276E-6, 2.793543E-2, 3.166766E-4, 
+        1.794936E-6, 4.155853E-5, 3.091273E-8, 4.075663E-4, 4.620180E-6, 
+        2.618737E-8, 6.063216E-7, 3.964163E-6, 4.493784E-8, 2.891790E-8, 
+        1.535389E-3, 1.740520E-5, 9.865342E-8, 2.284142E-6, 1.231187E-6, 
+        1.395677E-8};
+      std::vector<double> besCorrect {0.0, -2.030778, -4.061556, -6.092335, 
+        2.030778, 4.061556, -2.901112, -4.931890, -6.962669, -0.8703336, 
+        1.160444, -5.802224, -7.833002, -9.863781, -3.771445, -8.703336, 
+        -10.73411, -11.60444, 2.901112, 0.8703336, -1.160444, 4.931890, 
+        5.802224, 3.771445};
 
+      for ( auto i = 0; i < wts.size(); ++i ){ 
+        if ( i < wtsCorrect.size() ){ equal( wts[i], wtsCorrect[i] ); } 
+        else { equal( wts[i], 0.0 ); }
+      }
 
+      for ( auto i = 0; i < bes.size(); ++i ){ 
+        if ( i < besCorrect.size() ){ equal( bes[i], besCorrect[i] ); } 
+        else { equal( bes[i], 0.0 ); }
+      }
 
+      equal( wt, 3.0 );
+      equal( tbart, 407.4545311 );
 
-
-
-
-
-
+    } // THEN
   } // GIVEN
 } // TEST CASE
 
