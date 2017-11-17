8f9d687220d6c605d65aa069e5415499ce9b52dc
Amelia Trainer <ameliajo@mit.edu>
2017-11-17 17:30:41 -0500

    making more tests for add delta functions helper func

--- discre/discre_util/addDeltaFuncs.h
+++ discre/discre_util/addDeltaFuncs.h
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <vector>
 
-auto addDeltaFuncs( double twt, double dwf, std::vector<double>& bes,
-    std::vector<double>& betan, std::vector<double>& wts, 
-    std::vector<double>& sexpb, int n) {
+auto addDeltaFuncs( const double twt, const double dwf, 
+    const std::vector<double>& bes,
+    const std::vector<double>& betan, const std::vector<double>& wts, 
+    std::vector<double>& sexpb, const int n) {
 
   // Add the delta functions to the scattering law 
   // delta(0.0) is saved fro the incoherent elastic scattering
   int j, jj;
   double add;
-    std::cout << twt << std::endl;
   if ( twt <= 0.0 ){
     std::cout << "HERE" << std::endl;
     int m = 0, idone = 0;
@@ -20,14 +20,16 @@ auto addDeltaFuncs( double twt, double dwf, std::vector<double>& bes,
         idone = 1;
       } else {
         if ( bes[m-1] < 0.0 ){
+        std::cout << "HERE I AM" << std::endl;
           double be = -bes[m-1];
           if ( be < betan[betan.size()-2] ){
+        std::cout << "HERE I AM" << std::endl;
             double db = 1000;
             idone = 0;
             j = 0;
             while ( j < betan.size()-1 and idone == 0 ){
               j += 1;
-              int jj = j;
+              jj = j;
               if ( abs(be-betan[j-1] )>db ){
                 idone = 1;
               } else {
@@ -39,6 +41,7 @@ auto addDeltaFuncs( double twt, double dwf, std::vector<double>& bes,
             } else {
               add = 2 * wts[m-1]/(betan[jj-1]-betan[jj-2]);
             }
+            std::cout << "ADD:    " << add << std::endl; 
             add *= dwf;
             if ( add >= 1.0e-20 ){ sexpb[jj-2] += add; }
           }
--- discre/discre_util/addDeltaFuncs.test.cpp
+++ discre/discre_util/addDeltaFuncs.test.cpp
@@ -2,22 +2,61 @@
 #include "../../catch.hpp" 
 #include "addDeltaFuncs.h"
 
+
+void equal( double a, double b ){
+  if (b == 0.0){ 
+    REQUIRE( b-a < 1e-6 );
+    return;
+  }
+  REQUIRE ( std::abs( (a-b)/(b) ) < 1e-6 );
+}
+
+void equal_vec( std::vector<double> a, std::vector<double> b ){
+  REQUIRE( a.size() == b.size() );
+  for ( auto i = 0; i < a.size(); ++i ){
+    equal( a[i], b[i] );
+  }
+}
+
 TEST_CASE( "helper function to add delta functions to scattering law" ){
-  GIVEN( "inputs" ){
-    double twt = -0.1;
-    double dwf = 0.9;
-    int n = 1;
-    std::vector<double> bes ( 500, 0.0 ); 
-    std::vector<double> betan { 0.1, 0.2, 0.3, 0.5 };
-    std::vector<double> wts { 0.1, 0.2, 0.3, 0.5 };
-    std::vector<double> sexpb { 0.1, 0.2, 0.3, 0.5 };
+  double twt = -0.1, dwf = 0.9;
+  int n = 17;
+
+  std::vector<double> bes ( 500, 0.0 ), wts ( 500, 0.0 );
+
+  std::vector<double> betan { 0.1, 0.15, 0.30, 0.60, 1.20 }, 
+                      sexpb { 0.1, 0.2,  0.3,  0.4,  0.5  };
+
     double val = 9; int sign = -1;
     for ( auto i = 1; i < 20; ++i ){ 
       bes[i] = sign * val;
       sign *= -1; 
       val -= 1;
     }
-    for ( auto entry : bes ){ std::cout << entry << std::endl; }
+    int j = 0;
+    for ( auto i = 0.01; i > 1.0e-8; i = i / 10 ){
+      wts[j] = 4.0*i;
+      wts[j+1] = 3.0*i;
+      wts[j+2] = 2.0*i;
+      wts[j+3] = 1.0*i;
+      j = j + 4;
+    }
+
+  GIVEN( "all neg bes vals have larger mag. than the 2nd largeset beta val" ){
+  //  addDeltaFuncs( twt, dwf, bes, betan, wts, sexpb, n );
+    THEN( "no change to sexpb" ){
+      equal_vec( sexpb, {0.1,0.2,0.3,0.4,0.5} );
+    } // THEN
+  } // GIVEN
+
+  GIVEN( "some neg bes vals have smaller mag. than the 2nd largeset beta val" ){
+    sexpb = { 0.1, 0.2,  0.3,  0.4,  0.5  };
+    betan[3] = 2.3; betan[4] = 2.5;
+  //  for ( auto entry : bes ) { std::cout << entry << std::endl; }
     addDeltaFuncs( twt, dwf, bes, betan, wts, sexpb, n );
+    THEN( "sexpb is correctl amended" ){
+      for ( auto entry : sexpb ){ std::cout << entry << std::endl; }
+     // equal_vec( sexpb, {0.1,0.2,0.3002511747,0.4,0.5} );
+    } // THEN
   } // GIVEN
 } // TEST CASE
