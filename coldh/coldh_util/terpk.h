5eac5cf738329d64eaad2675054831a869193414
Amelia Trainer <ameliajo@mit.edu>
2017-11-22 18:55:44 -0500

    finished terpk interpolation and tests

--- coldh/coldh_util/terpk.h
+++ coldh/coldh_util/terpk.h
@@ -1,18 +1,11 @@
 #include <iostream>
 #include <vector>
 
-auto terpk( std::vector<double>& ska, std::vector<double>& nka, double& delta,
-  double& be ){
+auto terpk( std::vector<double>& ska, double& delta, double& x ){
   /* Interpolates in a table of ska(kappa) for a required kappa
    */
-  int i = be / delta;
-  if ( i < nka - 1 ){
-    double bt = i * delta;
-    double btp = bt + delta;
-    i += 1;
-    return ska[i-1] + (be-bt)*(ska[i]-ska[i-1])/(delta);
-  } else { 
-    return 1.0;
-  }
+  int i = x / delta;
+  return i < ska.size() - 1 ? ska[i] + (x-i*delta) * (ska[i+1]-ska[i]) / delta :
+                              1.0;
 }
 
--- coldh/coldh_util/terpk.test.cpp
+++ coldh/coldh_util/terpk.test.cpp
@@ -2,11 +2,46 @@
 #include "../../catch.hpp"
 #include "terpk.h"
 
+void equal( double a, double b ){
+  if( b == 0 ){ REQUIRE( (a-b) < 1e-6 ); }
+  if( b != 0 ){ REQUIRE ( std::abs( (a-b)/(b) ) < 1e-6 ); }
+}
+
 
 TEST_CASE( "terpk" ){
-  GIVEN( "inputs" ){
-  
+  GIVEN( "desired value within the range of the ska vector" ){
+    WHEN( "desired value is not a point in ska vector" ){
+      std::vector<double> ska {1.1, 2.2, 3.3, 5.5, 8.8, 13.13}; 
+      double delta = 0.2;
+      double be1 = 0.6;
+      double be2 = 0.01;
+      THEN( "interpolation is correctly performed" ){
+        equal( terpk( ska, delta, be1 ), 5.5 );
+        equal( terpk( ska, delta, be2 ), 1.155 );
+      } // THEN
+    } // WHEN
+    WHEN( "desired value is a point in ska vector" ){
+      std::vector<double> ska {1.1, 2.2, 3.3, 5.5, 8.8, 13.13}; 
+      double delta = 0.2;
+      double be1 = 0.0;
+      double be2 = 0.2;
+      double be3 = 0.8;
+      THEN( "corresponding point is returned" ){
+        equal( terpk( ska, delta, be1 ), 1.1 );
+        equal( terpk( ska, delta, be2 ), 2.2 );
+        equal( terpk( ska, delta, be3 ), 8.8 );
+      } // THEN
+    } // WHEN
+  } // GIVEN
 
-
+  GIVEN( "desired value is greater than the range of the ska vector" ){
+    std::vector<double> ska {1.1, 2.2, 3.3, 5.5, 8.8, 13.13}; 
+    double delta = 0.2;
+    double be1 = 1.0;
+    double be2 = 5.01;
+    THEN( "a value of 1.0 is returned" ){
+      equal( terpk( ska, delta, be1 ), 1.0 );
+      equal( terpk( ska, delta, be2 ), 1.0 );
+    } // THEN
   } // GIVEN
 } // TEST CASE
