1d14a41cc6dbd5315460973bcf5d063adc7cd11e
Amelia Trainer <ameliajo@mit.edu>
2017-11-25 17:38:17 -0500

    continuing work on sumh, adding test cawes

--- coldh/coldh_util/sumh.h
+++ coldh/coldh_util/sumh.h
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
+#include "sumh_util/cn.h"
+#include "sumh_util/sjbes.h"
 
 
 auto sumh(int j, int jp, double y){
   /* Does sum over Bessel functions and Clebsch-Gordon coefficients
    * for cold hydrogen or deuterium calculation.
    */ 
-  int imp, ipk1, mpk, ipk, n, n1;
+  int imp, ipk1, mpk, ipk, n, n1, imk;
   double sum1, sum2;
   if (j == 0) {
     sum2 = (sjbes(jp,y)*cn(j,jp,jp))*(sjbes(jp,y)*cn(j,jp,jp));
@@ -16,21 +18,20 @@ auto sumh(int j, int jp, double y){
   }
   else {
     sum1 = 0;
-    imk = iabs(j-jp) + 1;
+    imk = abs(j-jp) + 1;
     ipk1 = j + jp + 1;
     mpk = ipk1 - imk;
-    if (mpk.le.9) {
+    if (mpk <= 9) {
       ipk = ipk1;
     } else {
       ipk = imk + 9;
     }
-    for ( auto n = imk - 1; n < ipk; ++n ){
+    for ( auto n = imk; n <= ipk; ++n ){
       n1 = n - 1;
       sum1 = sum1 + (sjbes(n1,y)*cn(j,jp,n1))*(sjbes(n1,y)*cn(j,jp,n1));
     }
     sum2 = sum1;
    }
-   sumh = sum2;
-   return;
+   return sum2;
 
 }
