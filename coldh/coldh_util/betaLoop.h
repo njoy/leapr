70eca06a9625d902c197d13ceb7956fdd5b8058e
Amelia Trainer <ameliajo@mit.edu>
2017-11-28 17:27:14 -0500

    incorporate jprime things into betaLoop

--- coldh/coldh_util/betaLoop.h
+++ coldh/coldh_util/betaLoop.h
@@ -3,8 +3,11 @@
 #include <cmath>
 #include "betaLoop_util/jprimeLoop.h"
 
-auto betaLoop( std::vector<double>& betan, double alp, double x, int itemp,
-    int a, int law, std::vector<std::vector<std::vector<double>>>& sym_sab ){
+auto betaLoop( const std::vector<double>& betan, 
+  const std::vector<double>& rdbex, const std::vector<double>& bex,
+  const std::vector<double>& sex, const double& alphaVal, const double& wt, 
+  const double& tbart, const double& x, const double& y, int itemp, int nbx,
+  int a, int b, int law, std::vector<std::vector<std::vector<double>>>& sym_sab ){
   //--loop over all beta values
   //    results for positive beta go into ssp
   //    results for negative beta go into sym_sab
@@ -12,7 +15,7 @@ auto betaLoop( std::vector<double>& betan, double alp, double x, int itemp,
   int ifree = 0;
   int jjmax = 2 * betan.size() - 1;
   int k, jp, j;
-  double add, betap, be;
+  double add, betap, be, snlg, snlk;
   double pi = 3.14159265358979;
   for ( auto jj = 0; jj < jjmax; ++jj ){
     if ( jj < betan.size() ){
@@ -23,7 +26,7 @@ auto betaLoop( std::vector<double>& betan, double alp, double x, int itemp,
       be = betan[k-1];
     }
     int sn = 0;
-    int total = 0;
+    double total = 0;
 
     //--loop over all oscillators
     // para-h2: j=0,2,....; ortho-h2: j=1,3,....
@@ -38,50 +41,9 @@ auto betaLoop( std::vector<double>& betan, double alp, double x, int itemp,
       j = l - 1;
 
    //   call bt(j,pj,x)
+    snlg = jPrime( total, j, be, x, swe, pj, jj, bex, rdbex, sex, betan, alphaVal, wt, tbart, b, y, nbx, false );
+    snlk = jPrime( total, j, be, x, swo, pj, jj, bex, rdbex, sex, betan, alphaVal, wt, tbart, b, y, nbx, true  );
 
-      //--sum over even values of j-prime
-      double snlg = 0.0;
-      for ( auto lp = 1; lp < 10; lp = lp + 2 ){
-        jp = lp - 1;
-        betap=(-j*(j+1)+jp*(jp+1))*x/2;
-        int  tmp=(2*jp+1)*pj*swe*4;//*sumh(j,jp,y);
-        if (jj == 1 and tmp >= 1.0e-6) {
-          total += tmp;
-        }
-        double bn=be+betap;
-        if (ifree == 1) {
-          double ex=-(alp-abs(bn))*(alp-abs(bn))/(4*alp);
-          if (bn > 0.0) ex=ex-bn;
-          add = exp(ex) / sqrt(4*pi*alp);
-        }// else {
-         // add=sint(bn,bex,rdbex,sex,nbx,al,wt,tbart, betan,betan.size(),maxbb);
-       // }
-        snlg = snlg + tmp * add;
-      }
-      
-/*
-      //--sum over the odd values of j-prime
-      double snlk=0;
-      for ( auto lp = 2; lp < 10; lp = lp + 2 ){
-        jp=lp-1;
-        betap=(-j*(j+1)+jp*(jp+1))*x/2;
-        int tmp=(2*jp+1)*pj*swo*4*sumh(j,jp,y);
-        if (jj == 1 and tmp >= 1.0e-6) {
-          total += tmp;
-        }
-        double bn = be + betap;
-        if (ifree == 1) {
-          double ex=-(alp-abs(bn))*(alp-abs(bn))/(4*alp);
-          if (bn > 0.0) ex=ex-bn;
-          add = exp(ex) / sqrt(4*pi*alp);
-//        else
-  ///        add=sint(bn,bex,rdbex,sex,nbx,al,wt,tbart,betan,betan.size(),maxbb);
-        }
-        snlk = snlk + tmp * add;
-      }
-u
-
-    */
       //--continue the j loop
     //  sn=sn+snlg+snlk;
     }
--- coldh/coldh_util/betaLoop.test.cpp
+++ coldh/coldh_util/betaLoop.test.cpp
@@ -16,18 +16,22 @@ auto populateSymSab( const std::vector<double>& alpha, const std::vector<double>
 }
 
 
-
 TEST_CASE( "beta loop helper function" ){
   GIVEN( "inputs" ){
     std::vector<double> alpha { 0.1, 0.15, 0.30, 0.60, 1.20 };
     std::vector<double> betan { 0.1, 0.15, 0.30, 0.60, 1.20 };
-    double alp = 0.23;
-    double x = 0.85292696;
-    int itemp = 0;
-    int a = 0;
+    std::vector<double> rdbex { 1.6, 3.3, 6.6, 20, 5.0, 20, 6.6, 3.3, 1.6, 0.0, 0.0 };
+    std::vector<double> bex { -1.2, -0.6, -0.3, -0.15, -0.1, 0.1, 0.15, 0.3, 0.6, 1.2, 0.0 };
+    std::vector<double> sex { 5, 4, 3, 2, 1, 1, 1, 2, 2, 1, 5 };
+    double alpha_val = 0.1;
+    double x = 0.85292696, y = 0.35;
+    double wt = 2.3;
+    double tbart = 950;
+    int itemp = 0, nbx = 10;
+    int a = 0, b = 0;
     int law = 2;
     auto sym_sab = populateSymSab( alpha, betan );
-    betaLoop( betan, alp, x, itemp, a, law, sym_sab );
+    betaLoop( betan, rdbex, bex, sex, alpha_val, wt, tbart, x, y, itemp, nbx, a, b, law, sym_sab );
 
 
 
