fece58573c2052a5acfaec40c0d0eceee878b12f
Amelia Trainer <ameliajo@mit.edu>
2017-12-06 13:46:55 -0500

    working more on coldh betaloop tests, problem

--- coldh/coldh_util/betaLoop.test.cpp
+++ coldh/coldh_util/betaLoop.test.cpp
@@ -47,7 +47,8 @@ TEST_CASE( "beta loop helper function" ){
     sex { 0.1, 0.2, 0.3, 0.5, 0.8, 1.3, 2.1, 3.4, 5.5, 8.9, 14.4 },
     correct_sym_sab (25, 0.0), correct_sym_sab_2 (25, 0.0);
 
-  double alpha = 0.1, x = 3.2, y = 4.3, swe = 1.42, swo = 2.41, wt = 1.5, tbart = 820;
+  double alpha = 0.1, x = 3.2, y = 4.3, swe = 1.42, swo = 2.41, wt = 1.5, 
+         tbart = 820;
   int itemp = 0, nbx = 10, a = 0, ncold = 1;
 
   auto sym_sab   = populateSymSab( 5, 5, true );
@@ -56,85 +57,98 @@ TEST_CASE( "beta loop helper function" ){
 
   GIVEN( "molecular translations are assumed to not be free" ){
     bool free = false;
-    betaLoop( betan, rdbex, bex, sex, alpha, wt, tbart, x, y, swe, swo, 
-      itemp, nbx, a, ncold, free, sym_sab, sym_sab_2 );
 
+    //------------------------------------------------------------------------
     correct_sym_sab = { 0.882913433, 0.649954077, 0.550346135, 0.394586983, 
       0.202842464, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 
       22, 23, 24, 25  };
-
     correct_sym_sab_2 = { 0.8829134, 0.9175721, 1.029927, 1.297595, 2.059706, 0, 
       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     
     THEN( "output scattering laws are correct" ){
+      betaLoop( betan, rdbex, bex, sex, alpha, wt, tbart, x, y, swe, swo, 
+        itemp, nbx, a, ncold, free, sym_sab, sym_sab_2 );
       equal_vec_mega_vec( sym_sab, correct_sym_sab );
       equal_vec_mega_vec( sym_sab_2, correct_sym_sab_2 );
     } // THEN
-  } // GIVEN
-
-
-
-  GIVEN( "inputs" ){
-    std::vector<double> bex { -1.2, -0.6, -0.3, -0.15, -0.1, 0.1, 0.15, 0.3, 
-      0.6, 1.2, 0.0 };
-    bool free = false;
-    betaLoop( betan, rdbex, bex, sex, alpha, wt, tbart, x, y, swe, swo, 
-      itemp, nbx, a, ncold, free, sym_sab, sym_sab_2 );
+    //------------------------------------------------------------------------
+    //------------------------------------------------------------------------
+    
+    bex = { -1.2, -0.6, -0.3, -0.15, -0.1, 0.1, 0.15, 0.3, 0.6, 1.2, 0.0 };
 
     correct_sym_sab = { 1.02763700, 0.395247529, 0.237150220, 0.158101736, 
       8.66802482E-2, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
       21, 22, 23, 24, 25 }; 
-
     correct_sym_sab_2 = { 1.02763700, 1.66002658, 2.68765983, 4.34768245, 
       6.44628349, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     
-    
     THEN( "output scattering laws are correct" ){
+      betaLoop( betan, rdbex, bex, sex, alpha, wt, tbart, x, y, swe, swo, 
+        itemp, nbx, a, ncold, free, sym_sab, sym_sab_2 );
       equal_vec_mega_vec( sym_sab, correct_sym_sab );
       equal_vec_mega_vec( sym_sab_2, correct_sym_sab_2 );
     } // THEN
-  } // GIVEN
+    //------------------------------------------------------------------------
+    //------------------------------------------------------------------------
 
-  bex = { -1.2, -0.6, -0.3, -0.15, -0.1, 0.1, 0.15, 0.3, 0.6, 1.2, 0.0 };
-  sex = { 5, 4, 3, 2, 1, 1, 1, 2, 2, 1, 5 };
-
-  GIVEN( "inputs" ){
+    sex = { 5, 4, 3, 2, 1, 1, 1, 2, 2, 1, 5 };
     x = 3.2; y = 4.3; swe = 1.42; swo = 2.41; wt = 1.5; tbart = 820;
-    bool free = false;
-    betaLoop( betan, rdbex, bex, sex, alpha, wt, tbart, x, y, swe, swo, 
-      itemp, nbx, a, ncold, free, sym_sab, sym_sab_2 );
 
     correct_sym_sab = { 0.790490930, 1.58097809, 2.37146522, 3.16195249, 
       3.70600979, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 
       22, 23, 24, 25 };
-
     correct_sym_sab_2 = { 0.790490930, 0.790490910, 1.58097789, 1.58097781, 
       0.790490664, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-     
+ 
     THEN( "output scattering laws are correct" ){
+      betaLoop( betan, rdbex, bex, sex, alpha, wt, tbart, x, y, swe, swo, 
+        itemp, nbx, a, ncold, free, sym_sab, sym_sab_2 );
       equal_vec_mega_vec( sym_sab, correct_sym_sab );
       equal_vec_mega_vec( sym_sab_2, correct_sym_sab_2 );
     } // THEN
-  } // GIVEN
+    //------------------------------------------------------------------------
+    //------------------------------------------------------------------------
 
-  GIVEN( "inputs" ){
     x = 0.85; y = 0.35; swe = 0.32; swo = 0.87; wt = 2.3; tbart = 950;
-
-    bool free = false;
-    betaLoop( betan, rdbex, bex, sex, alpha, wt, tbart, x, y, swe, swo, 
-      itemp, nbx, a, ncold, free, sym_sab, sym_sab_2 );
 
     correct_sym_sab = { 1.70316846, 3.37656089, 5.04886254, 6.69968895, 
       7.86359423, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 
       22, 23, 24, 25 };
     correct_sym_sab_2 = { 1.70316846, 1.70258735, 3.37159930, 3.36200407, 
       1.68666467, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-    
-    
+ 
     THEN( "output scattering laws are correct" ){
+      betaLoop( betan, rdbex, bex, sex, alpha, wt, tbart, x, y, swe, swo, 
+        itemp, nbx, a, ncold, free, sym_sab, sym_sab_2 );
       equal_vec_mega_vec( sym_sab, correct_sym_sab );
       equal_vec_mega_vec( sym_sab_2, correct_sym_sab_2 );
     } // THEN
+  } // GIVEN
+
+  GIVEN( "molecular translations are assumed to be free" ){
+    bool free = true;
+
+    bex = { -1.2, -0.6, -0.3, -0.15, -0.1, 0.1, 0.15, 0.3, 0.6, 1.2, 0.0 };
+    
+      std::cout << "\n\n\n\n\n\n"<< std::endl;
+
+    //------------------------------------------------------------------------
+    correct_sym_sab =  { 0.518806235, 0.575763580, 0.554572267, 0.410837243, 
+      9.16701822E-2, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+      21, 22, 23, 24, 25 }; 
+    correct_sym_sab_2 = { 0.518806235, 0.495564322, 0.410837296, 0.225472850, 2.76505342E-2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    THEN( "output scattering laws are correct" ){
+      betaLoop( betan, rdbex, bex, sex, alpha, wt, tbart, x, y, swe, swo, 
+        itemp, nbx, a, ncold, free, sym_sab, sym_sab_2 );
+      equal_vec_mega_vec( sym_sab, correct_sym_sab );
+      std::cout << sym_sab[0][1][0] << std::endl;
+      std::cout << sym_sab_2[0][1][0] << std::endl;
+    //  equal_vec_mega_vec( sym_sab_2, correct_sym_sab_2 );
+    } // THEN
+    //------------------------------------------------------------------------
+    //------------------------------------------------------------------------
+ 
+
 
   } // GIVEN
 } // TEST CASE
