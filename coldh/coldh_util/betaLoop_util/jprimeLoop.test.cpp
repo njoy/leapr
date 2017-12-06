720713b4cc7ec6e26a95fef9583fb8a163ac19a5
Amelia Trainer <ameliajo@mit.edu>
2017-12-05 21:33:13 -0500

    finished jprime tests

--- coldh/coldh_util/betaLoop_util/jprimeLoop.test.cpp
+++ coldh/coldh_util/betaLoop_util/jprimeLoop.test.cpp
@@ -46,50 +46,78 @@ TEST_CASE( "jprime loop" ){
     } // WHEN
 
     WHEN( "molecular translations are assumed to be free (free = true)" ){
+      double total1 = 3; j = 4; be = 1.23; x = 0.1; swe = 0.2; pj = 2.4; 
+             jj = 2; al = 2.0; wt = 0.05; y = 0.3;
+
+      auto out1 = jPrime( total1, j, be, x, swe, pj, jj, bex, rdbex, sex, betan,
+                         al, wt, tbart, y, nbx, false, true );
+
+      double total2 = 5.0; jj = 0;
+
+      auto out2 = jPrime( total2, j, be, x, swe, pj, jj, bex, rdbex, sex, betan,
+                         al, wt, tbart, y, nbx, false, true );
+
+      THEN( "output matches expectation" ){
+        equal( total1, 3.0 );
+        equal( out1,   1.9988998E-2 );
+        equal( total2, 6.8634280542 );
+        equal( out2,   1.9988998E-2 );
+
+      } // THEN
 
     } // WHEN
   } // GIVEN
 
 
   GIVEN( "jprime loop for odd values" ){
-    WHEN( "inputs" ){
-    double total = 0.02, wt = 2.3, be = -1.2, al = 0.1, x = 0.85, 
+
+    std::vector<double> 
+    bex { -1.2, -0.6, -0.3, -0.15, -0.1, 0.1, 0.15, 0.3, 0.6, 1.2, 0.0 },
+    rdbex{ 1.666667, 3.33333, 6.6667, 20, 5, 20, 6.6667, 3.3333, 1.66667, 0, 0 },
+    betan { 0.1, 0.15, 0.3, 0.6, 1.2 },
+    sex(11,0.0);
+
+    double wt = 2.3, be = -1.2, al = 0.1, x = 0.85, 
            swo = 0.87, pj = 0.48, y = 0.35, tbart = 950.0;
+
     int j = 1, jj = 0, nbx = 10;
-    std::vector<double> bex { -1.2, -0.6, -0.3, -0.15, -0.1, 0.1, 0.15, 0.3, 
-      0.6, 1.2, 0.0 };
-    std::vector<double> rdbex { 1.6666666, 3.3333333, 6.6666666, 20.0, 5.0, 
-      20.0, 6.66666, 3.333333, 1.666666, 0.0, 0.0 };
-    std::vector<double> sex { 5.0, 4.0, 3.0, 2.0, 1.0, 1.0, 1.721415952, 
-      2.2224546, 2.1952465, 1.5059710, 5.0 };
-    std::vector<double> betan { 0.1, 0.15, 0.3, 0.6, 1.2 };
 
-    double snlk = jPrime( total, j, be, x, swo, pj, jj, bex, 
+    WHEN( "molecular translations are assumed to not be free (free = false)" ){
+      sex = { 5, 4, 3, 2, 1, 1, 1.72141595, 2.222455, 2.195246, 1.505971, 5 };
+      double total1 = 0.02;
+      double out1 = jPrime( total1, j, be, x, swo, pj, jj, bex, 
+          rdbex, sex, betan, al, wt, tbart, y, nbx, true, false );
+
+      THEN( "output matches expectation" ){
+        equal( out1,   8.01757644 );
+        equal( total1, 1.62384370 );
+      } // THEN
+
+      double total2 = 0.0; x = 0.8; swo = 0.9; pj = 0.4; y = 0.3;
+      for ( auto i = 0; i < sex.size(); ++i ){ sex[i] = i + 1; }
+      double out2= jPrime( total2, j, be, x, swo, pj, jj, bex, 
         rdbex, sex, betan, al, wt, tbart, y, nbx, true, false );
-    equal( snlk,  8.01757644 );
-    equal( total, 1.62384370 );
-    
-    } // GIVEN
 
-  WHEN( "other inputs" ){
-    double total = 0.0, wt = 2.3, be = -1.2, al = 0.1, x = 0.8, 
-           swo = 0.9, pj = 0.4, y = 0.3, tbart = 950.0;
-    int j = 1, jj = 0, nbx = 10;
-    std::vector<double> bex { -1.2, -0.6, -0.3, -0.15, -0.1, 0.1, 0.15, 0.3, 
-      0.6, 1.2, 0.0 };
-    std::vector<double> rdbex { 1.6666666, 3.3333333, 6.6666666, 20.0, 5.0, 
-      20.0, 6.66666, 3.333333, 1.666666, 0.0, 0.0 };
-    std::vector<double> betan { 0.1, 0.15, 0.3, 0.6, 1.2 };
-    std::vector<double> sex(11,0.0);
-    for ( auto i = 0; i < sex.size(); ++i ){
-      sex[i] = i + 1;
-    }
-    double snlk = jPrime( total, j, be, x, swo, pj, jj, bex, 
-        rdbex, sex, betan, al, wt, tbart, y, nbx, true, false );
-    equal( snlk,  1.397417419 );
-    equal( total, 1.397570948 );
+      THEN( "output matches expectation" ){
+        equal( out2,   1.397417419 );
+        equal( total2, 1.397570948 );
+      } // THEN
 
-  } // WHEN
+
+    } // WHEN
+
+    WHEN( "molecular translations are assumed to not be free (free = false)" ){
+      for ( auto i = 0; i < sex.size(); ++i ){ sex[i] = 2 * (i + 1); }
+      double total = 0.02; wt = 2.05; y = 0.35; swo = 0.47; total = 5.0;
+      double out = jPrime( total, j, be, x, swo, pj, jj, bex, 
+          rdbex, sex, betan, al, wt, tbart, y, nbx, true, true );
+
+      THEN( "output matches expectation" ){
+        equal( out,   0.16137096895 );
+        equal( total, 5.86644429347 );
+      } // THEN
+    } // WHEN
+
   } // GIVEN
 
 } // TEST CASE
