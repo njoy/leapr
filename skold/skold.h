ece2eb4e079d9834eb0a528d0737f5f2a7784ce8
Amelia Trainer <ameliajo@mit.edu>
2017-12-11 17:07:36 -0500

    comtinued work on skold

--- skold/skold.h
+++ skold/skold.h
@@ -3,44 +3,38 @@
 #include "../coldh/coldh_util/terpk.h"
 #include "skold_util/terp1.h"
 
-auto skold( double cfrac, int itemp, double temp, 
+auto skold( double cfrac, int itemp, double tev,
   const std::vector<double>& alpha, const std::vector<double>& beta, 
-  const std::vector<double>& skappa,
-  int ntempr, double awr, int lat, int nka, double dka, double scaling,
+  const std::vector<double>& skappa, int ntempr, double awr, int lat, int nka, 
+  double dka, double scaling,
   std::vector<std::vector<std::vector<double>>>& symSab ){
   /* use skold approximation to add in the effects
    * of intermolecular coherence.
    */
   int i, j, k, kk, nal, ibeta;
-  double tev, sc, amass, al, sk, ap, be, ss, s1, s2;
-  double sum0, sum1, ff1l, ff2l, bel, ff1, ff2, waven;
+  double sc, sk, ap, be, ss, s1, s2, waven;
   std::vector<double> scoh ( 1000, 0.0 );
 
-  double amassn = 1.008664904; 
-  double amu = 1.6605402e-24;
-  double bk = 8.617385e-5;
-  double hbar = 1.05457266e-27;
-  double ev = 1.60217733e-12;
+  double amassn = 1.008664904,    amu = 1.6605402e-24, bk = 8.617385e-5,
+         hbar   = 1.05457266e-27, ev  = 1.60217733e-12;
 
-  
   // apply the skold approximation
-  tev = bk * abs(temp);
   sc = 1;
   if (lat == 1) sc = 0.0253 / tev;
-  amass = awr * amassn * amu;
   for ( auto b = 0; b < beta.size(); ++b ){
     for ( auto a = 0; a < alpha.size(); ++a ){
-      al = alpha[a] * scaling;
-      waven = 1.0e-8 * sqrt(2*amass*tev*ev*al)/hbar;
+      waven = 1.0e-8 * sqrt(2*awr*amassn*amu*tev*ev*alpha[a]*scaling)/hbar;
       sk = terpk(skappa,dka,waven);
       ap = alpha[a] / sk;
       for ( auto a2 = 0; a2 < alpha.size(); ++a2 ){
         kk = a2;
-        if (ap < alpha[a2]) break;
+        if (ap < alpha[a2]){ break; }
       }
       if (kk == 0) kk = 1;
-      terp1(alpha[kk-1],symSab[kk-1][b][itemp],alpha[kk],symSab[kk][b][itemp],ap,scoh[a],5);
-      scoh[a] = scoh[a] * sk;
+
+      terp1( alpha[kk-1], symSab[kk-1][b][itemp], alpha[kk], 
+             symSab[kk][b][itemp], ap,s coh[a], 5 );
+      scoh[a] *= sk;
     }
     for ( auto a = 0; a < alpha.size(); ++a ){
       symSab[a][b][itemp] = (1-cfrac)*symSab[a][b][itemp]+cfrac*scoh[a];
--- skold/skold.test.cpp
+++ skold/skold.test.cpp
@@ -40,22 +40,35 @@ auto populateSymSab( int a1, int b1 ){
 
 
 TEST_CASE( "skold" ){
-    double cfrac = 0.3;
-    auto symsab = populateSymSab( 5, 5 );
-    double temp = 200.0, awr = 8.9;
-    int nalpha = 5, nbeta = 5, ntempr = 1, itemp = 0;
-    int lat = 3, nka = 3;
-    double dka = 2.3, scaling = 1.0;
-    std::vector<double> alpha  { 0.10, 0.20, 0.40, 0.80, 1.60 };
-    std::vector<double> beta   { 0.10, 0.15, 0.30, 0.60, 1.20 };
-    std::vector<double> skappa { 1.10, 2.20, 3.30, 4.40, 5.50 };
-    std::vector<double> rightSymSab { 0.7748423, 1.695805, 2.725674, 
-      3.834036, 5, 4.525767, 5.779165, 7.133967, 8.547886, 10, 9.056398, 
-      10.95454, 12.81736, 14.66346, 16.5, 16.62805, 19.17417, 21.51546, 
-      23.77780, 26, 21, 22, 23, 24, 25 };
-  GIVEN( "inputs" ){
-    skold( cfrac, itemp, temp, alpha, beta, skappa, ntempr, awr, lat, nka, dka, scaling, symsab );
-    equal_vec_mega_vec( symsab, rightSymSab );
+  double cfrac = 0.3, tev = 0.0172348, awr = 8.9, dka = 2.3, scaling = 1.0;
+  int nalpha = 5, nbeta = 5, ntevr = 1, itev = 0, lat = 3, nka = 3;
+  auto symsab = populateSymSab( 5, 5 );
+  std::vector<double> alpha { 0.10, 0.20, 0.40, 0.80, 1.60 }, 
+    beta { 0.1, 0.15, 0.3, 0.6, 1.2 }, skappa { 1.1, 2.2, 3.3, 4.4, 5.5 }, 
+    rightSymSab { 0.7748423, 1.695805, 2.725674, 3.834036, 5, 4.525767, 
+    5.779165, 7.133967, 8.547886, 10, 9.056398, 10.95454, 12.81736, 14.66346,
+    16.5, 16.62805, 19.17417, 21.51546, 23.77780, 26, 21, 22, 23, 24, 25 };
+
+  GIVEN( "medium sized skappa values" ){
+    WHEN( "dka is a bit large" ){
+      THEN( "the scattering law is correctly changed" ){
+        skold( cfrac, itev, tev, alpha, beta, skappa, ntevr, awr, lat, nka, 
+          dka, scaling, symsab );
+        equal_vec_mega_vec( symsab, rightSymSab );
+      } // THEN
+    } // WHEN
+    WHEN( "dka is really big" ){
+      THEN( "the scattering law is correctly changed" ){
+        dka = 25.0; cfrac = 1.3;
+        rightSymSab = { 0.6489166, 1.614790, 2.691405, 3.827429, 5, 3.545016, 
+          5.406339, 7.019986, 8.535378, 10, 11.53434, 13.04755, 14.54618, 
+          16.03430, 17.51459, 19.78249, 21.38301, 22.97954, 24.57282, 26.16343,
+          29.73225, 31.50051, 33.26723, 35.03263, 36.79689 };
+        skold( cfrac, itev, tev, alpha, beta, skappa, ntevr, awr, lat, nka, 
+          dka, scaling, symsab );
+        equal_vec_mega_vec( symsab, rightSymSab );
+      } // THEN
+    } // WHEN
   } // GIVEN
 
   GIVEN( "small values in skappa" ){
@@ -64,11 +77,11 @@ TEST_CASE( "skold" ){
       THEN( "no change to scattering law" ){
         for ( int i = 0; i < rightSymSab.size(); ++i ){ rightSymSab[i] = i+1; }
         dka = 0.03;
-        skold( cfrac, itemp, temp, alpha, beta, skappa, ntempr, awr, lat, nka,
+        skold( cfrac, itev, tev, alpha, beta, skappa, ntevr, awr, lat, nka,
           dka, scaling, symsab );
         equal_vec_mega_vec( symsab, rightSymSab );
         dka = 1.03;
-        skold( cfrac, itemp, temp, alpha, beta, skappa, ntempr, awr, lat, nka, 
+        skold( cfrac, itev, tev, alpha, beta, skappa, ntevr, awr, lat, nka, 
           dka, scaling, symsab );
         equal_vec_mega_vec( symsab, rightSymSab );
       } // THEN
@@ -81,7 +94,7 @@ TEST_CASE( "skold" ){
         rightSymSab = { 0.7362570, 1.435067, 2.134116, 2.833354, 3.532743, 
           4.248877, 4.946653, 5.644851, 6.343379, 7.042169, 11, 12, 13, 14, 15, 
           16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
-        skold( cfrac, itemp, temp, alpha, beta, skappa, ntempr, awr, lat, nka, 
+        skold( cfrac, itev, tev, alpha, beta, skappa, ntevr, awr, lat, nka, 
           dka, scaling, symsab );
         equal_vec_mega_vec( symsab, rightSymSab );
         
@@ -90,7 +103,7 @@ TEST_CASE( "skold" ){
         rightSymSab = { -0.1428862, -0.4480389, -0.7521607, -1.055464, 
           -1.358109, -1.588199, -1.897833, -2.205644, -2.512024, -2.817265, 11,
           12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
-        skold( cfrac, itemp, temp, alpha, beta, skappa, ntempr, awr, lat, nka, 
+        skold( cfrac, itev, tev, alpha, beta, skappa, ntevr, awr, lat, nka, 
           dka, scaling, symsab );
         equal_vec_mega_vec( symsab, rightSymSab );
 
@@ -104,7 +117,7 @@ TEST_CASE( "skold" ){
           -1.368090, -1.602126, -1.911557, -2.219208, -2.525467, -2.830620, 
           -3.032806, -3.348865, -3.661900, -3.972597, -4.281455, -4.441460, 
           -4.767483, -5.088515, -5.405735, -5.719990, 21, 22, 23, 24, 25 };
-        skold( cfrac, itemp, temp, alpha, beta, skappa, ntempr, awr, lat, nka, 
+        skold( cfrac, itev, tev, alpha, beta, skappa, ntevr, awr, lat, nka, 
           dka, scaling, symsab );
         equal_vec_mega_vec( symsab, rightSymSab );
 
