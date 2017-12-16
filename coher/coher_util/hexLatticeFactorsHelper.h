e661936219ed92c808d073c5fb2a5720d2d7d9e6
Amelia Trainer <ameliajo@mit.edu>
2017-12-16 11:29:00 -0500

    finished hex lattice helper testing, starting cleanup

--- coher/coher_util/hexLatticeFactorsHelper.h
+++ coher/coher_util/hexLatticeFactorsHelper.h
@@ -5,33 +5,32 @@
 
 
 auto hexLatticeFactorsHelper( int& k, const double& tsq, 
-  const double& tsqx, std::vector<double>& b, const int& ifl, int& i,
+  const double& tsqx, std::vector<double>& b, const int& ifl,
   const double& wint, const int& nw, const double& eps, 
   double f ){
 
   double tau;
-
   if (k <= 0 or tsq <= tsqx) {
     k=k+1;
     if ((2*k) > nw) std::cout << "ERROR" << std::endl; 
-    b[ifl+2*k-2-1]=tsq;
-    b[ifl+2*k-1-1]=f;
+    b[ifl+2*k-3] = tsq;
+    b[ifl+2*k-2] = f;
   }
   else {
-    i=0;
-    int idone=0;
-    while (i < k and idone == 0){
-      i=i+1;
-      if (tsq >= b[ifl+2*i-2-1] and tsq < (1+eps)*b[ifl+2*i-2-1]) {
-        b[ifl+2*i-1-1]=b[ifl+2*i-1-1]+f;
-        idone=1;
+    int i = 0;
+    bool done = false;
+    while (i < k and not done ){
+      i += 1;
+      if (tsq >= b[ifl+2*i-3] and tsq < (1+eps)*b[ifl+2*i-3]) {
+        b[ifl+2*i-2] += f;
+        done = true;
       } 
     }
-    if (idone == 0) {
-      k=k+1;
+    if (not done) {
+      k += 1;
       if ((2*k) > nw) std::cout << "storage exceeded" << std::endl;
-      b[ifl+2*k-2-1]=tsq;
-      b[ifl+2*k-1-1]=f;
+      b[ifl+2*k-3] = tsq;
+      b[ifl+2*k-2] = f;
     }
   } 
 } 
--- coher/coher_util/hexLatticeFactorsHelper.test.cpp
+++ coher/coher_util/hexLatticeFactorsHelper.test.cpp
@@ -5,7 +5,7 @@
 
 void equal( double a, double b ){
   if (b == 0.0){ 
-    REQUIRE( b-a < 1e-6 );
+    REQUIRE( abs(b-a) < 1e-6 );
     return;
   }
   REQUIRE ( std::abs( (a-b)/(b) ) < 1e-6 );
@@ -21,28 +21,28 @@ void equal_vec( std::vector<double> a, std::vector<double> b ){
 
 TEST_CASE( "Function to Compute Hexagonal Lattice Factors" ){
   int lat = 2, k = 0, ifl = 1, i = 0, nw = 6;
-  double tsq = 0.1, tsqx  = 9.6, wint = 0, eps = 5e-5, f = 3;
+  double tsq = 0.1, tsqx  = 9.6, wint = 0, eps = 5e-2, f = 3;
   std::vector<double> b ( 60000, 0.0 );
 
   GIVEN( "k is not positive or tsq <= tsqx" ){
     THEN( "b's first two entries are populted with tsq and f, respectively" ){
-      hexLatticeFactorsHelper( k, tsq, tsqx, b, ifl, i, wint, nw, eps, f );
+      hexLatticeFactorsHelper( k, tsq, tsqx, b, ifl, wint, nw, eps, f );
 
-      equal( b[0], 0.1 ); equal( b[1], 3.0 ); equal( k, 1 ); equal( i, 0 );
+      equal( b[0], 0.1 ); equal( b[1], 3.0 ); equal( k, 1 );
       for ( auto i = 2; i < b.size(); ++i ){ equal( b[i], 0 ); }
 
       f = 100; k = 0;
 
-      hexLatticeFactorsHelper( k, tsq, tsqx, b, ifl, i, wint, nw, eps, f );
+      hexLatticeFactorsHelper( k, tsq, tsqx, b, ifl, wint, nw, eps, f );
 
-      equal( b[0], 0.1 ); equal( b[1], 100 ); equal( k, 1 ); equal( i, 0 );
-      for ( auto i = 2; i < b.size(); ++i ){ equal( b[i], 0 ); }
+      equal( b[0], 0.1 ); equal( b[1], 100 ); equal( k, 1 ); 
+     for ( auto i = 2; i < b.size(); ++i ){ equal( b[i], 0 ); }
 
 
       tsq = 50; k = 0;
 
-      hexLatticeFactorsHelper( k, tsq, tsqx, b, ifl, i, wint, nw, eps, f );
-      equal( b[0], 50 ); equal( b[1], 100 ); equal( k, 1 ); equal( i, 0 );
+      hexLatticeFactorsHelper( k, tsq, tsqx, b, ifl, wint, nw, eps, f );
+      equal( b[0], 50 ); equal( b[1], 100 ); equal( k, 1 );
       for ( auto i = 2; i < b.size(); ++i ){ equal( b[i], 0 ); }
 
 
@@ -51,15 +51,24 @@ TEST_CASE( "Function to Compute Hexagonal Lattice Factors" ){
 
   GIVEN( "k is positive and tsq > tsqx" ){
     WHEN( "loop is able to finish" ){
-      THEN( "we're in the first situation" ){
+      THEN( "we're in the 2st situation, f goes in b (only 1 val added)" ){
         tsq = 30.1, tsqx = 25;
-        k = 1; f = 4.5e-2;
-        hexLatticeFactorsHelper( k, tsq, tsqx, b, ifl, i, wint, nw, eps, f );
+        k = 2; f = 4.5e-2;
+        b[0] = 10; b[1] = 20; b[2] = 30; b[3] = 40;
+        hexLatticeFactorsHelper( k, tsq, tsqx, b, ifl, wint, nw, eps, f );
 
-//        equal( b[0], 0 ); equal( b[1], 0 ); equal( b[2], 700 ); 
-//        equal( b[3], 4.5e-2); equal( k, 2 ); equal( i, 1 );
-//        for ( auto i = 4; i < b.size(); ++i ){ equal( b[i], 0 ); }
+        equal( b[0], 10 ); equal( b[1], 20 ); equal( b[2], 30 ); 
+        equal( b[3], 40.045 ); equal( k, 2 ); 
+        for ( auto i = 4; i < b.size(); ++i ){ equal( b[i], 0 ); }
 
+
+        tsq = 10.1, tsqx = 5;
+        k = 1; f = 1.5;
+        b[0] = 10; b[1] = 20; b[2] = 30; b[3] = 40;
+        hexLatticeFactorsHelper( k, tsq, tsqx, b, ifl, wint, nw, eps, f );
+        equal( b[0], 10 ); equal( b[1], 21.5 ); equal( b[2], 30 ); 
+        equal( b[3], 40 ); equal( k, 1 );
+        for ( auto i = 4; i < b.size(); ++i ){ equal( b[i], 0 ); }
       } // THEN
     } // WHEN
     
@@ -67,11 +76,20 @@ TEST_CASE( "Function to Compute Hexagonal Lattice Factors" ){
       THEN( "we're in the second situation" ){
         tsq = 700, tsqx = 500;
         k = 1; f = 4.5e-2;
-        hexLatticeFactorsHelper( k, tsq, tsqx, b, ifl, i, wint, nw, eps, f );
+        hexLatticeFactorsHelper( k, tsq, tsqx, b, ifl, wint, nw, eps, f );
 
         equal( b[0], 0 ); equal( b[1], 0 ); equal( b[2], 700 ); 
-        equal( b[3], 4.5e-2); equal( k, 2 ); equal( i, 1 );
+        equal( b[3], 4.5e-2 ); equal( k, 2 ); 
         for ( auto i = 4; i < b.size(); ++i ){ equal( b[i], 0 ); }
+
+        tsq = 63, tsqx = 50;
+        k = 1; f = 1.5;
+        b[0] = 10; b[1] = 20; b[2] = 30; b[3] = 40; b[4] = 50; b[5] = 60;
+        hexLatticeFactorsHelper( k, tsq, tsqx, b, ifl, wint, nw, eps, f );
+
+        equal( b[0], 10 ); equal( b[1], 20 ); equal( b[2], 63 ); 
+        equal( b[3], 1.5 ); equal( b[4], 50 ); equal( b[5], 60 );equal( k, 2 ); 
+        for ( auto i = 6; i < b.size(); ++i ){ equal( b[i], 0 ); }
 
 
       } // THEN
