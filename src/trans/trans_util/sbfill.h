e60276c6fb78b47895da89e0b04c7bfa1491edc5
Amelia Trainer <ameliajo@mit.edu>
2018-06-28 16:24:10 -0600

    starting improving some trans stuff for ranges related stuff

--- src/trans/trans_util/sbfill.h
+++ src/trans/trans_util/sbfill.h
@@ -3,38 +3,45 @@
 #include <cmath>
 #include <range/v3/all.hpp>
 
-void sbfill(std::vector<double>& sb, int nbt, double delta, double be, 
-  std::vector<double>& s,std::vector<double>& betan, int ndmax){
+template <typename floatT, typename arrayT>
+void sbfill(arrayT& sb, int nbt, floatT delta, floatT be, arrayT& s, 
+  arrayT& betan, int ndmax){
 
-  double bmin = -be - (nbt-1) * delta;
-  double bmax = -be + (nbt-1) * delta + delta * 0.01;
+  floatT bmin = -be - (nbt-1) * delta,
+         bmax = -be + (nbt-1) * delta + delta * 0.01;
 
   if ( 1 + (bmax-bmin) / delta > ndmax){ 
     throw std::exception();
   }
   
-  int i = 0;
+  //int i = 0;
   unsigned int j = betan.size()-1;
-  double current, toLeft, bet = bmin, slim = -225e0;
-  //bool indexInRange = false; 
+  floatT current, toLeft, /*bet = bmin,*/ slim = -225e0;
   
-  while (bet < bmax){
-    std::cout << bet << std::endl;
-    double b = std::abs(bet);
+
+  // We are going to have round up(bmax-bmin/delta) many iterations. 
+  // So track the iteration number. And zip this with a beta range,
+  // which goes from bmin --> bmax in steps of size delta
+  auto iRange   = ranges::view::iota(0,int((bmax - bmin)/delta)+1);
+  auto iBetaRange = ranges::view::zip( 
+                      iRange, 
+                      iRange | ranges::view::transform( [bmin,delta](int i){
+                                 return bmin + (1.0*i*delta); } ) );
+
+
+  for ( const auto& tupleEntry : iBetaRange ){
+    int i = std::get<0>(tupleEntry);
+    floatT bet = std::get<1>(tupleEntry);
+    floatT b = std::abs(bet);
+
     do {
-      if ( b >  betan[j] and (j+1) != betan.size() )    { ++j; }
+      if ( b >  betan[j] and j+1 != betan.size() )    { ++j; }
       if ( b <= betan[j] and  b < betan[j-1] and j > 1 ){ --j; }
     }  
     while (
          (b >  betan[j] or ( b < betan[j-1] and j > 1 ) ) and 
-         (b <= betan[j] or ( j+1 != betan.size() ) ) );
-
-    /*
-    if ( b > betan[j] and  j + 1 == betan.size() ){ 
-      indexInRange = b < 1.00001 * betan[j] ? true : false;
-    }
-    if ( b <= betan[j] ){ indexInRange = true; }
-    */
+         (b <= betan[j] or ( j+1 != betan.size() ) ) 
+    );
 
     
     if ( (b > betan[j] and ( j+1!=betan.size() or b >= 1.00001*betan[j]) ) ){
@@ -47,15 +54,8 @@ void sbfill(std::vector<double>& sb, int nbt, double delta, double be,
       if (bet > 0) { sb[i] = sb[i] - bet; }
       if ( sb[i] > slim ){ sb[i] = exp(sb[i]); }
     }
-
-    i = i + 1;
-    bet = bet + delta;
   }
-  return;
-  auto betRange = ranges::view::iota(0,int((bmax - bmin)/delta)+1) 
-                | ranges::view::transform([bmin,delta](int i){
-                    return bmin + (double(i)*delta); } );
-  std::cout << betRange << std::endl;
+
 }
 
 
--- src/trans/trans_util/terps.h
+++ src/trans/trans_util/terps.h
@@ -1,22 +1,25 @@
 
 
-double terps( const std::vector<double>& sd, int nsd, const double& delta, 
-  const double& x_val ){
+template <typename floatT, typename arrayT>
+floatT terps( const arrayT& sd, int nsd, const floatT& delta, 
+  const floatT& xVal ){
   // Because we need another interpolation function
 	
-  double yL, yR, y_val, min = -225.0;
+  floatT yL, yR, yVal, min = -225.0;
 
   // Index of desired x value
-  int xi = x_val/delta;
+  int xi = xVal/delta;
+
   // If out of bounds, return 0.0
   if ( xi < 0 or xi > nsd - 1 ){ return 0.0; }
     
-  double xL = xi * delta;
+  floatT xL = xi * delta;
+
   // Don't take the log of a negative number
   yL = sd[xi]   <  0.0 ? min : log( sd[xi]   );
   yR = sd[xi+1] <= 0.0 ? min : log( sd[xi+1] );
     
-  y_val = yL + ( x_val - xL ) * ( yR - yL ) / ( delta );
+  yVal = yL + ( xVal - xL ) * ( yR - yL ) / ( delta );
 
-  return y_val >= min ? exp(y_val) : 0.0;
+  return yVal >= min ? exp(yVal) : 0.0;
 }
