2e30bed6cbe26fa0b560382bd105c316edd1081a
Amelia Trainer <ameliajo@mit.edu>
2018-07-06 14:19:51 -0400

    got sum0 initially working

--- src/contin/contin_util/checkMoments.h
+++ src/contin/contin_util/checkMoments.h
@@ -14,6 +14,7 @@ auto checkMoments( const double& sc, const std::vector<double>& alpha,
   auto alphaBeta = ranges::view::cartesian_product( 
                      ranges::view::iota(0,int(alpha.size())), 
                      ranges::view::iota(0,int(beta.size())));
+  
   /*
 		     */
 
@@ -71,9 +72,16 @@ auto checkMoments( const double& sc, const std::vector<double>& alpha,
     return ssmVal;  }, ssct2, alphaBeta, ssm );
 
 
-  auto ff1_2 = ranges::view::zip_with( [](auto ff2,auto be){
-                 return ff2*exp(-be); }, ff2_2, be2 );
+  auto ff1_2 = ranges::view::zip( ff2_2, alphaBeta )
+             | ranges::view::transform([sc,beta](auto t){
+                 auto ff2 = std::get<0>(t);
+                 auto ab = std::get<1>(t);
+                 auto be = beta[std::get<1>(ab)]*sc;
+                 //return be;});//*exp(-be); });
+                 return ff2*exp(-be); });
+//RANGES_FOR( auto t, ff1_2 ){ std::cout << t <<std::endl; }
 
+std::cout << std::endl;
   auto be_bel = ranges::view::concat(ranges::view::single(0.0),be2 )
               | ranges::view::sliding(2);
   auto ff1l_2 = ranges::view::concat(ranges::view::single(0.0),ff1_2 )
@@ -83,9 +91,22 @@ auto checkMoments( const double& sc, const std::vector<double>& alpha,
               | ranges::view::sliding(2);
 
   auto sum0_2 = ranges::view::zip_with(                  
-		  [](auto be, auto ff1l, auto ff2l){ 
-                    return (be[1]-be[0])*(ff1l[0]+ff1l[1]+ff2l[0]+ff2l[1])*0.5;},
-                  be_bel,ff1l_2,ff2l_2);
+		  [alpha,beta,sc,f0,arat](auto ab, auto ff1l, auto ff2l){ 
+                    auto bel = beta[std::get<1>(ab)-1]*sc;
+                    auto be  = beta[std::get<1>(ab)]*sc;
+                    auto al  = alpha[std::get<0>(ab)]*sc/arat;
+                    if (std::get<1>(ab) == 0){ return 0.0; }
+                    return (be-bel)*(ff1l[0]+ff1l[1]+ff2l[0]+ff2l[1])*0.5;},///(1.0-exp(-al*f0));},
+                    //return (1.0-exp(-al*f0));
+      //sum0 = sum0/(1-exp(-al*f0));
+                  alphaBeta,ff1l_2,ff2l_2);
+  auto sum0_2b = ranges::view::cartesian_product(ff1l_2,be_bel);
+//RANGES_FOR( auto t, sum0_2b ){ std::cout << std::get<1>(t)[0] << "   " << std::get<1>(t)[1]  << "    " << std::get<0>(t)[0] << "    " << std::get<0>(t)[1]  <<std::endl; }
+		  //[](auto be, auto ff1l, auto ff2l){ 
+                  //  return (be[1]-be[0])*(ff1l[0]+ff1l[1]+ff2l[0]+ff2l[1])*0.5;},
+                  //be_bel,ff1l_2,ff2l_2);
+
+
 
   auto sum1_2 = ranges::view::zip_with(                  
 		  [](auto be, auto ff1l, auto ff2l){ 
@@ -93,20 +114,21 @@ auto checkMoments( const double& sc, const std::vector<double>& alpha,
                                            ff2l[0]*be[0]+ff2l[1]*be[1]);},
                   be_bel,ff1l_2,ff2l_2);
 
-  RANGES_FOR( auto b, ff1l_2 ){ std::cout << b[0] << "     " << b[1] << std::endl;}
+  //RANGES_FOR( auto b, ff1l_2 ){ std::cout << b[0] << "     " << b[1] << std::endl;}
 
 double countDouble = 0.0;
 RANGES_FOR( auto x, sum0_2 ){ 
   countDouble += x;
 }
 
+/*
 RANGES_FOR( auto x, ranges::view::zip( ff1l_2,ff2l_2 )){
     auto ff1l_val = std::get<0>(x);
     auto ff2l_val = std::get<1>(x);
-    //std::cout << ff1l_val << std::endl;
     //std::cout << ff2l_val << std::endl;
     std::cout << ff1l_val[0] << "  " << ff1l_val[1] << "  " << ff2l_val[0] << "   " << ff2l_val[1] << std::endl;
   }
+  */
 
   {
   std::vector<std::vector<double>> ssm (alpha.size(),std::vector<double>(beta.size(),0.0));
@@ -139,25 +161,26 @@ RANGES_FOR( auto x, ranges::view::zip( ff1l_2,ff2l_2 )){
         ff2 = ssm[a][b];
         ff1 = ssm[a][b]*exp(-be);
         //std::cout << ff1l << "      " << ff1  << std::endl;
-        std::cout << ff1l << "   " << ff1 << "   " << ff2l << "   " << ff2 << std::endl;
+        //std::cout << bel << "   " << be << "    " << ff1l << "   " << ff1 << std::endl;//"   " << ff2l << "   " << ff2 << std::endl;
+        //std::cout << ff1 << "     " << ff1_2[counter++] << std::endl;
         //ff0 = ssm[a][b][itemp]*exp(-be/2);   // This isn't used either
         if (b > 0) {
           sum0 = sum0+(be-bel)*(ff1l+ff2l+ff1+ff2)/2.0;
           sum1 = sum1+(be-bel)*(ff2l*bel+ff2*be-ff1l*bel-ff1*be)/2.0;
         }
         else {
-          sum0 = 0;
-          sum1 = 0;
+          sum0 += 0;
+          sum1 += 0;
         }
-        //std::cout << sum0<< "       " << sum0_2[counter++] << std::endl;
+        std::cout << sum0<< "       " << sum0_2[counter++] << std::endl;
         ff1l = ff1;
         ff2l = ff2;
         bel = be;
       }
-      //std::cout << "sum0:  " << sum0 << "     alpha:  " << a << std::endl;
       //std::cout << "sum1:  " << sum1 << std::endl;
       
       //sum0 = sum0/(1-exp(-al*f0));
+      //std::cout << "sum0:  " << (1-exp(-al*f0)) << "     alpha:  " << a << std::endl;
       //sum1 = sum1/al/tbeta;
       //sum0 = 0; sum1= 0;
     }
@@ -165,12 +188,14 @@ RANGES_FOR( auto x, ranges::view::zip( ff1l_2,ff2l_2 )){
   ++counter;
   }
  
-  return;
+  std::cout << std::endl;
+  std::cout << sum0_2 << std::endl;
+  std::cout << std::endl;
 auto sum = ranges::accumulate(sum0_2, 0.0);
   std::cout << sum << std::endl;
+  return;
   RANGES_FOR( auto x, ranges::view::zip(alpha,sum0_2)){ std::cout << std::get<1>(x) << "      " << std::get<0>(x) << std::endl;
       }
-  std::cout << sum0_2 << std::endl;
 
   std::cout << sum1_2 << std::endl;
 
