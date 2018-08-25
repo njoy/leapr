07ee2e98be6c3cbf3ee4aad46f3139a5134a7fac
Amelia Trainer <ameliajo@mit.edu>
2018-08-25 09:43:00 -0400

    added more test things for cdf so i can then actually tuck it into real code

--- src/cdf/cdf.h
+++ src/cdf/cdf.h
@@ -23,12 +23,13 @@ auto calc_eq_14(int b, A ssm){
 }
 
 template <typename A> 
-auto calc_eq_18(int a, int b, A ssm ){
+auto calc_eq_15(int a, int b, A ssm ){
   double numerator   = ssm(a,b,0);
   double denominator = 0;
   for ( int aPrime = 0; aPrime < ssm.dimension(0); ++aPrime ){
     denominator += ssm(aPrime,b,0);
   } 
+  if (denominator < 1e-50){ return 0.0; } 
   return numerator/denominator;
 }
 
@@ -42,30 +43,29 @@ auto calc_eq_16(int b, A ssm){
   return sum;
 }
 
+
 template <typename F, typename I, typename A>
-auto cdf(I ntempr, I nphon, 
-  I lat, 
-  F delta, F twt, F c, F tbeta, A alpha, A beta, A temp, A rho
-  ){
+auto cdf(I ntempr, I nphon, I lat, F delta, F twt, F c, F tbeta, A alpha, 
+  A beta, A temp, A rho ){
 
-    auto out = leaprWaterSpecific( ntempr, nphon, lat, alpha, beta, temp, 
-                                   delta, rho, twt, c, tbeta );
+  auto out = leaprWaterSpecific( ntempr, nphon, lat, alpha, beta, temp, delta, 
+                                 rho, twt, c, tbeta );
 
-    double lambda_s = std::get<0>(out),
-           t_eff    = std::get<1>(out);
+  double lambda_s = std::get<0>(out),
+         t_eff    = std::get<1>(out);
 
-    auto ssm = std::get<2>(out);
+  auto ssm = std::get<2>(out);
 
-    A eq14Values(beta.size()), eq16Values(beta.size());
+  A eq14Values(beta.size()), eq16Values(beta.size());
+  auto eq15Values = ssm;
+  auto eq17Values = ssm;
 
-    for ( int i = 0; i < int(beta.size()); ++i ){ 
-      //std::cout << beta[i] << "           " << calc_eq_14(i,ssm) << std::endl;
-      //std::cout << calc_eq_14(i,ssm) << "          " << calc_eq_16(i,ssm) << std::endl;
-      eq14Values[i] = calc_eq_14(i,ssm);
-      eq16Values[i] = calc_eq_16(i,ssm);
+  for ( int i = 0; i < int(beta.size()); ++i ){ 
+    eq14Values[i] = calc_eq_14(i,ssm);
+    eq16Values[i] = calc_eq_16(i,ssm);
+    for ( int j = 0; j < int(alpha.size()); ++j ){
+      eq15Values(i,j,0) = calc_eq_15(i,j,ssm);
     }
-    //std::cout << std::setprecision(16) << calc_eq_14(25,ssm) << std::endl;
-    //std::cout << std::endl;
-
-  return std::make_tuple(eq14Values,eq16Values);
+  }
+  return std::make_tuple(eq14Values,eq15Values,eq16Values,eq17Values);
 }
