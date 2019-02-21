cbec688aa79fc32014c82a0594be3b976c0f3770
Amelia Trainer <ameliajo@mit.edu>
2019-02-20 21:39:50 -0500

    added tests for the summing over Tn

--- src/simple/continuous/sumOVerTn.h
+++ src/simple/continuous/sumOVerTn.h
@@ -2,39 +2,27 @@
 #include <iostream>
 
 
-double fact(int n){
-  if (n == 0){ return 1; }
-  return n*fact(n-1);
-}
-
+double fact(int n){ return n == 0 ? 1 : n*fact(n-1); }
 
 void print(std::vector<double> x){
   std::cout << std::endl;
   for (auto& y : x){ std::cout << y << " "; }
   std::cout << std::endl;
 }
+
 void print(double x){
-  std::cout << std::endl;
-  std::cout << x << std::endl;
+  std::cout << std::endl << x << std::endl;
 }
 
 
 
-
-
 template <typename V, typename F>
-auto sumOverTn( const V& alphas, const V& betas, const V& T1, 
-  const F& lambda_s ){
-  alphas = {2,4};
-  betas = {0,1,2,3,4};
-  T1 = {1,2,3,2,1};
-  lambda_s = 0.5;
+V sumOverTn( const V& alphas, const V& betas, const V& T1, const F& lambda_s,
+  int N = 0){
+
   int nAlpha = alphas.size();
   int nBeta  = betas.size();
 
-
-  nAlpha = alphas.size();
-  nBeta  = betas.size();
   V Tn(nBeta);
   std::copy( T1.begin(), T1.begin() + nBeta, Tn.begin() );
 
@@ -43,12 +31,13 @@ auto sumOverTn( const V& alphas, const V& betas, const V& T1,
   F factorialTerm = 1.0;
   V sab(nAlpha*nBeta,0.0);
   // Perform sum in Eq. 523
-  for ( int n = 0; n < 2; ++n ){
+  for ( int n = 0; n < N; ++n ){
     for ( int a = 0; a < nAlpha; ++a ){
       F alpha = alphas[a];
       F alphaTerm = exp(-alpha*lambda_s)*(1.0/fact(n))*std::pow(alpha*lambda_s,n);
-      if (n == 0 and betas[0] == 0){
-        sab[a*nBeta+0] = alphaTerm;
+      // Accounting for our T0(b) = delta(b) term
+      if (n == 0){
+        if (betas[0] == 0){ sab[a*nBeta+0] = alphaTerm; } 
         continue;
       }
       for ( int b = 0; b < nBeta; ++b ){
@@ -57,16 +46,11 @@ auto sumOverTn( const V& alphas, const V& betas, const V& T1,
     } // alpha loop
     if (n == 0){ continue; }
     Tn = convolve(betas,T1,Tn);
+    //print(n);
+    //print(Tn);
   } // summation loop
 
-  //print(sab[0*nBeta+0]);
-  //print(sab[0*nBeta+1]);
-  print(exp(-1)*3);
-  print(sab[0*nBeta+2]);
-
-
-
-
+  return sab;
 
 }
 
--- src/simple/continuous/test/sumOverTn.test.cpp
+++ src/simple/continuous/test/sumOverTn.test.cpp
@@ -4,13 +4,59 @@
 
 
 TEST_CASE( "sumOverTn" ){
-  GIVEN( "" ){
-    THEN( "" ){
+  GIVEN( "Only one iteration is performed (only want T0)" ){
+    THEN( "We only see the effects of the T0(b) = delta(b) term" ){
       std::vector<double> alphas {2,4}, betas {0,1,2,3,4}, T1 {1,2,3,2,1};
-
-
+      double lambda_s = 0.5;
+      int N = 1;
+      auto sab = sumOverTn(alphas,betas,T1,lambda_s,N);
+      REQUIRE( exp(-1) == Approx(sab[0*betas.size()+0]).epsilon(1e-6) );
+      REQUIRE( exp(-2) == Approx(sab[1*betas.size()+0]).epsilon(1e-6) );
+      for ( size_t b = 1; b < betas.size(); ++b ){
+        REQUIRE( 0.0 == Approx(sab[0*betas.size()+b]).epsilon(1e-6) );
+        REQUIRE( 0.0 == Approx(sab[1*betas.size()+b]).epsilon(1e-6) );
+      }
     } // THEN
   } // GIVEN
+  GIVEN( "Two iterations are performed (T0 and T1)" ){
+    THEN( "T0(b) = delta(b) and T1 which is already defined are used" ){
+      std::vector<double> alphas {2,4}, betas {0,1,2,3,4}, T1 {1,2,3,2,1};
+      double lambda_s = 0.5;
+      int N = 2;
+      auto sab = sumOverTn(alphas,betas,T1,lambda_s,N);
+      REQUIRE( 2.0*exp(-1) == Approx(sab[0*betas.size()+0]).epsilon(1e-6) );
+      REQUIRE( 2.0*exp(-1) == Approx(sab[0*betas.size()+1]).epsilon(1e-6) );
+      REQUIRE( 3.0*exp(-1) == Approx(sab[0*betas.size()+2]).epsilon(1e-6) );
+      REQUIRE( 2.0*exp(-1) == Approx(sab[0*betas.size()+3]).epsilon(1e-6) );
+      REQUIRE( 1.0*exp(-1) == Approx(sab[0*betas.size()+4]).epsilon(1e-6) );
+
+      REQUIRE( 3.0*exp(-2) == Approx(sab[1*betas.size()+0]).epsilon(1e-6) );
+      REQUIRE( 4.0*exp(-2) == Approx(sab[1*betas.size()+1]).epsilon(1e-6) );
+      REQUIRE( 6.0*exp(-2) == Approx(sab[1*betas.size()+2]).epsilon(1e-6) );
+      REQUIRE( 4.0*exp(-2) == Approx(sab[1*betas.size()+3]).epsilon(1e-6) );
+      REQUIRE( 2.0*exp(-2) == Approx(sab[1*betas.size()+4]).epsilon(1e-6) );
+    } // THEN
+  } // GIVEN
+  GIVEN( "Three iterations are performed (T0, T1, T2)" ){
+    THEN( "" ){
+      std::vector<double> alphas {2,4}, betas {0,1,2,3,4}, T1 {1,2,3,2,1};
+      double lambda_s = 0.5;
+      int N = 3;
+      auto sab = sumOverTn(alphas,betas,T1,lambda_s,N);
+      //REQUIRE( 2.0*exp(-1) == Approx(sab[0*betas.size()+0]).epsilon(1e-6) );
+      //REQUIRE( 2.0*exp(-1) == Approx(sab[0*betas.size()+1]).epsilon(1e-6) );
+      //REQUIRE( 3.0*exp(-1) == Approx(sab[0*betas.size()+2]).epsilon(1e-6) );
+      //REQUIRE( 2.0*exp(-1) == Approx(sab[0*betas.size()+3]).epsilon(1e-6) );
+      //REQUIRE( 1.0*exp(-1) == Approx(sab[0*betas.size()+4]).epsilon(1e-6) );
+
+      //REQUIRE( 3.0*exp(-2) == Approx(sab[1*betas.size()+0]).epsilon(1e-6) );
+      //REQUIRE( 4.0*exp(-2) == Approx(sab[1*betas.size()+1]).epsilon(1e-6) );
+      //REQUIRE( 6.0*exp(-2) == Approx(sab[1*betas.size()+2]).epsilon(1e-6) );
+      //REQUIRE( 4.0*exp(-2) == Approx(sab[1*betas.size()+3]).epsilon(1e-6) );
+      //REQUIRE( 2.0*exp(-2) == Approx(sab[1*betas.size()+4]).epsilon(1e-6) );
+    } // THEN
+  } // GIVEN
+
 } // TEST
 
 
